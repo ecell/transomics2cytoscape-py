31f6b9989c7e7c39343353973e3dc9753556b2af
Kozo Nishida <kozo.nishida@gmail.com>
2022-05-17 18:06:00 +0900

    Update transomics2cytoscape.py

--- transomics2cytoscape/transomics2cytoscape.py
+++ transomics2cytoscape/transomics2cytoscape.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import py4cytoscape as p4c
 from Bio.KEGG import REST
 
 def create3Dnetwork(networkDataDir, networkLayers, stylexml):
@@ -7,13 +8,14 @@ def create3Dnetwork(networkDataDir, networkLayers, stylexml):
 def createTransomicEdges(suid, transomicEdges):
     pass
 
-
 def createTransomicEdge(row, nt, edgetbl, suid):
     pass
 
 def importLayer(row):
     getKgml(row.array[2])
-    kgml = row.array[2]
+    kgml = row.array[2] + ".xml"
+    print("Importing " + kgml)
+    res = p4c.import_network_from_file(kgml)
 
 def getKgml(pathwayID):
     f = open("pathwayID" + ".xml", "a")
