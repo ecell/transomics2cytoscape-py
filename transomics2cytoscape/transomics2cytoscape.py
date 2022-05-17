8214324b0d557cbcb52790912300605b07e0dfe2
Kozo Nishida <kozo.nishida@gmail.com>
2022-05-17 16:58:07 +0900

    Update transomics2cytoscape.py

--- transomics2cytoscape/transomics2cytoscape.py
+++ transomics2cytoscape/transomics2cytoscape.py
@@ -1,10 +1,14 @@
-def create3Dnetwork(networkDataDir, networkLayers, stylexml):
-    pass
+import pandas as pd
 
+def create3Dnetwork(networkDataDir, networkLayers, stylexml):
+    layerTable = pd.read_table(networkLayers, sep="\t")
 
 def createTransomicEdges(suid, transomicEdges):
     pass
 
 
 def createTransomicEdge(row, nt, edgetbl, suid):
+    pass
+
+def importLayer(row):
     pass
