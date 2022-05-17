ff1472e92b26405f4869ad136b9fea4ed1d2d423
Kozo Nishida <kozo.nishida@gmail.com>
2022-05-17 17:13:21 +0900

    Update transomics2cytoscape.py

--- transomics2cytoscape/transomics2cytoscape.py
+++ transomics2cytoscape/transomics2cytoscape.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from Bio.KEGG import REST
 
 def create3Dnetwork(networkDataDir, networkLayers, stylexml):
     layerTable = pd.read_table(networkLayers, sep="\t")
@@ -12,3 +13,9 @@ def createTransomicEdge(row, nt, edgetbl, suid):
 
 def importLayer(row):
     pass
+
+def getKgml(pathwayID):
+    f = open("pathwayID" + ".xml", "a")
+    f.write(REST.kegg_get("eco01100", "kgml").read())
+    f.close()
+    
